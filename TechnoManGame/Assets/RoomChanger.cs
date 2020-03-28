51199d196610cf228ced6423aae84b66963f3b02
BDDimitrov18 <58163537+BDDimitrov18@users.noreply.github.com>
2020-03-28 03:44:41 +0200

    MadeDungeonGenerator

---
+++ TechnoManGame/Assets/RoomChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomChanger : MonoBehaviour
+{
+    private Vector2 position;
+    private Vector2 size;
+    private Vector3 rotation;
+    public void MakeChange(GameObject temp,GameObject parent)
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+        foreach (Transform child in temp.transform)
+        {
+            position = child.position;
+            size = child.localScale;
+            Instantiate(child.gameObject,position,child.rotation,parent.transform);
+        }
+    }
+}
---
+++ TechnoManGame/Assets/Scripts/MapSpriteSelector.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSpriteSelector : MonoBehaviour {
+	
+	public GameObject 	spU, spD, spR, spL,
+			spUD, spRL, spUR, spUL, spDR, spDL,
+			spULD, spRUL, spDRU, spLDR, spUDRL;
+	public bool up, down, left, right;
+	public int type; // 0: normal, 1: enter
+	public Color normalColor, enterColor;
+	Color mainColor;
+
+    public GameObject rend;
+    public LevelGeneration gen;
+
+    private Vector2 position;   
+    private Vector3 rotation;
+
+    void Start () {
+        //rend = GetComponent<SpriteRenderer>();
+        mainColor = normalColor;
+        PickSprite();
+        Instantiate(rend, transform.position, transform.rotation);
+    }
+	void PickSprite(){ //picks correct GameObject based on the four door bools
+        if (up)
+        {
+            if (down)
+            {
+                if (right)
+                {
+                    if (left)
+                    {
+                        rend = spUDRL;
+                    }
+                    else
+                    {
+                        rend = spDRU;
+                    }
+                }
+                else if (left)
+                {
+                    rend = spULD;
+                }
+                else
+                {
+                    rend = spUD;
+                }
+            }
+            else
+            {
+                if (right)
+                {
+                    if (left)
+                    {
+                        rend = spRUL;
+                    }
+                    else
+                    {
+                        rend = spUR;
+                    }
+                }
+                else if (left)
+                {
+                    rend = spUL;
+                }
+                else
+                {
+                    rend = spU;
+                }
+            }
+            return;
+        }
+        if (down)
+        {
+            if (right)
+            {
+                if (left)
+                {
+                    rend = spLDR;
+                }
+                else
+                {
+                    rend = spDR;
+                }
+            }
+            else if (left)
+            {
+                rend = spDL;
+            }
+            else
+            {
+                rend = spD;
+            }
+            return;
+        }
+        if (right)
+        {
+            if (left)
+            {
+                rend = spRL;
+            }
+            else
+            {
+                rend = spR;
+            }
+        }
+        else
+        {
+            rend = spL;
+            Debug.Log("asd");
+        }
+    }
+
+}
